108cc97ca454a2560f02a6be1cfd6c2d14789ce6
Pavlo365 <pavelpop134@gmail.com>
2026-03-09 11:46:21 +0200

    Update all work

--- src/masks/__init__.py
+++ src/masks/__init__.py
@@ -1,3 +1,4 @@
-from src.masks.masks import get_mask_account, get_mask_card_number  # noqa: F401
+from src.masks.masks import get_mask_account  # noqa: F401
+from src.masks.masks import get_mask_card_number
 
 all = ["get_mask_card_number", "get_mask_account"]
