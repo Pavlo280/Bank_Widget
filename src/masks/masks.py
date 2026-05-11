3ec394e38d4a4843dd399c1785864e6592599c04
Pavlo365 <pavelpop134@gmail.com>
2026-05-11 17:29:46 +0300

    Исправил логику маскировки и тесты

--- src/masks/masks.py
+++ src/masks/masks.py
@@ -11,7 +11,16 @@ def get_mask_card_number(card_number: int) -> str:
         Строка с замаскированным номером карты.
     """
     card_str = str(card_number)
-    return f"{card_str[:4]} {card_str[4:6]}** **** {card_str[12:]}"
+    if not card_str.isdigit():
+        return "Ошибка"
+    if len(card_str) == 16:
+        return f"{card_str[:4]} {card_str[4:6]}** **** {card_str[12:]}"
+    elif len(card_str) == 15:
+        return f"{card_str[:4]} {card_str[4:6]}**** *{card_str[11:]}"
+    elif len(card_str) > 16:
+        return f"{card_str[:4]} {card_str[4:6]}** **** **** {card_str[-3:]}"
+    else:
+        return "Ошибка"
 
 
 def get_mask_account(account_number: int) -> str:
@@ -19,12 +28,11 @@ def get_mask_account(account_number: int) -> str:
 
     Принимает номер счёта в виде числа и возвращает строку
     с маской по правилу: **XXXX (последние 4 цифры).
-
-    Args:
-        account_number: Номер банковского счёта.
-
-    Returns:
-        Строка с замаскированным номером счёта.
     """
-    account_str = str(account_number)
-    return f"**{account_str[-4:]}"
+    account_str = str(account_number).replace(" ", "")
+    if not account_str.isdigit():
+        return "Ошибка"
+    if len(account_str) > 4 :
+        return f"**{account_str[-4:]}"
+    else:
+        return "Ошибка"
